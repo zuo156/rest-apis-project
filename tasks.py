5e1529f9a51044a4d4bf14e6f084b7068b30b973
左盈 <zuoying@hidarieis-MBP.lan>
2023-10-04 23:37:40 -0400

    rq email on render.com

--- tasks.py
+++ tasks.py
@@ -1,24 +1,33 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
 
-def send_simple_message(to, subject, body):
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
      f"https://api.mailgun.net/v3/{DOMAIN}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
 		data={"from": f"Ying Zuo <mailgun@{DOMAIN}>",
 			"to": [to],
 			"subject": subject,
-			"text": body}
+            "text": body,
+			"html": html
+   }
   )
     
 def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "Successfully signed up",
-        f"Hi {username}! You have successfully signed up to the Stores REST API."
+        f"Hi {username}! You have successfully signed up to the Stores REST API.",
+        render_template("email/action.html",username=username)
     )
